235a30e09c925c6cac63fd6137d142bde1871ee8
Reales Myles <rclarence123@gmail.com>
2024-03-31 20:55:34 -0500

    Initial Commit

---
+++ StateBinding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateBinding
+//
+//  Created by Reales Rectoro Myles Clarence on 31/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateBinding/StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingApp.swift
+//  StateBinding
+//
+//  Created by Reales Rectoro Myles Clarence on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

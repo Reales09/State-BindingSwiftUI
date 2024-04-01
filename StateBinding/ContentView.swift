01c149e79774628ace0e1faf33fa6622ed348849
Reales Myles <rclarence123@gmail.com>
2024-03-31 21:04:47 -0500

    State

--- StateBinding/ContentView.swift
+++ StateBinding/ContentView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var  x = ""
+    var x1 = 2
+    
+    func suma() ->Int{
+        x = "Cambiando su valor" // variable viene de fuera se usa state
+        var x2 = 2 // variable se crea dentro de la funcion no se usa el state
+        x2 = 4
+        return x2
+    }
     var body: some View {
         VStack {
             Image(systemName: "globe")
